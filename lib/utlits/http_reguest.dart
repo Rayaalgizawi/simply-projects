12d80a0d97599e0ca24ee640696b5c4f7db0593f
Raya Ahmad Fadel ALGhazawi <96349318+Rayaalgizawi@users.noreply.github.com>
2022-05-01 00:37:50 +0300

    Update http_reguest.dart

--- lib/utlits/http_reguest.dart
+++ lib/utlits/http_reguest.dart
@@ -2,21 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as https;
 import 'dart:convert';
 
-// used provider state management
+
 class API extends ChangeNotifier{
 
-//
+// create method get api used json 
   getCountryData () async{
 
     const _apiUrl = 'https://countriesnow.space/api/v0.1/countries/info?returns=currency,flag,unicodeFlag,dialCode';
     final data =await https.get(Uri.parse(_apiUrl)) ;
 
+    // check all connections
     if(data.statusCode == 200 ){
+      // decode data 
       var allDataBody = json.decode(data.body);
       return await allDataBody;
 
     }else {
-      // Error 
+      // if Error print msg 
       return print('connection Error please Fix ') ;
     }
 
